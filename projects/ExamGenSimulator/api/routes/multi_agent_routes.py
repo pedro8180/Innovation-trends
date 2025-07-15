b486819cca3fc7d8653d8499f34dbb1c88b170a6
pedro8180 <rodriguezpedro1709@gmail.com>
2025-07-15 14:38:07 -0600

    updates

--- projects/ExamGenSimulator/api/routes/multi_agent_routes.py
+++ projects/ExamGenSimulator/api/routes/multi_agent_routes.py
@@ -5,16 +5,17 @@ from models.query import Query
 router = APIRouter()
 
 @router.post("/agents")
-async def call_supervisor_agent(request: Query):
+async def call_agent(request: Query):
 
-    agent = QuestionGeneratorAgent.get_question_generator_agent() #SupervisorAgent.create_supervisor_agent()
+    #agent = QuestionGeneratorAgent.get_question_generator_agent() #
 
+    agent = SupervisorAgent.create_supervisor_agent()
     #query = "Give me a set of questions for practicing for my certification exam."
 
     messages = {"messages": [{"role":"user", "content": request.query}]}
 
     response = agent.invoke(messages)
 
-    formatted_response = response['structured_response']
+    #formatted_response = response['structured_response']
     
-    return formatted_response
+    return response['messages'][-1].content
